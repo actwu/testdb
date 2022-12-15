5a782da5ca85704cd01cec210147e731aa38a5a7
Laucsap <110190482+actwu@users.noreply.github.com>
2022-12-15 18:44:15 +0800

    Add files via upload

--- sets/config.js
+++ sets/config.js
@@ -11,8 +11,8 @@
     dbtype = "macros",
     subject="/s/",
 
-    icsdb ="AKfycbwnWcnY14eUCzQcA9B-UGW9GSkQX917EOPKRvSC8NgqD6qa8PHUHosRLGIl9pWzB_nB",
-    icsoutput="AKfycbwOzLOFk6tAD3-Ax4ZLTbxzedT2rTBo1WlDzNwpU3sUDECdQ2QuK6XwpRdSKMxS4iEA",
+    icsdb ="AKfycbzViRVR0lmLr7p3nWc-tdHyEG-PwM0T42exeunsRQCL2ZXVyVfInddqrIDicE3Ftzyx",
+    icsoutput=icsdb,
     dbpost = [`${type}script.${site}${dbtype}${subject}${icsdb}/exec`],
 
     read_reference = dbget = [`${type}script.${site}${dbtype}${subject}${icsoutput}/exec`],
